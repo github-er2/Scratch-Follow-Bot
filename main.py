aac402d9d2c4495c436f4a5807cbebcd7baa90b5
OneParallaxAway <121917927+github-er2@users.noreply.github.com>
2023-01-17 13:18:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ while True:
   user = login.connect_user(username="ANYUSER")
 
   latestcomment = str(user.comments(limit=1, page=1))
-
-  partitioned_string = latestcomment.partition("'User': '")
-  partone = partitioned_string[2]
-  parttwo = partone.partition("',")
-  commenter = parttwo[0]
-
+  commenter = latestcomment["USER"]
+  
   print(commenter)
   login.follow_user(username=str(commenter))
